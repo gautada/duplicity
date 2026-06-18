9af68313db700e74b11ebdfa921c478838ef401c
Adam T. Gautier <1045171+gautada@users.noreply.github.com>
2026-06-17 21:42:58 -0400

    Added new sh

--- duplicity-syncjob.sh
+++ duplicity-syncjob.sh
@@ -1,8 +1,8 @@
-#!/bin/ash
+#!/bin/sh
 
 # This script synchronizes the source to target directories 
 # to provide a copy for the duplicity script to run to encrypt the backup 
 # and push to the backup to destination and offsite.
 
-USER=$(awk -F':' -v uid=1001 '$3 == uid { print $1 }' /etc/passwd)
-/bin/su $USER -c "/usr/bin/duplicity-backup"
+USER="$(awk -F':' -v uid=1001 '${3} == uid { print "${1}" }' /etc/passwd)"
+/bin/su "${USER}" -c "/usr/bin/duplicity-backup"
