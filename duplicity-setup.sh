e53a1879946be9d503d031230dc4f4658782a2b6
Adam T. Gautier <1045171+gautada@users.noreply.github.com>
2026-06-26 17:41:06 -0400

    Full setup

--- duplicity-setup.sh
+++ duplicity-setup.sh
@@ -6,15 +6,12 @@
 . /usr/bin/duplicity-common
 
 # Backup additionally requires the signer passphrase.
-require SIGNER_FINGERPRINT
-require ENCRYPTER_FINGERPRINT
+require SIGNER_PASSPHRASE
 
-# This scrip should setup gpc for use with duplicity
+# This script should setup gpc for use with duplicity
 /usr/bin/gpg --batch --yes --import "/mnt/volumes/secrets/${ENCRYPTER_FINGERPRINT}.asc"
 /usr/bin/gpg --batch --yes --import "/mnt/volumes/secrets/${SIGNER_FINGERPRINT}.asc"
 /usr/bin/gpg --list-keys --fingerprint "${ENCRYPTER_FINGERPRINT}"
 /usr/bin/gpg --list-secret-keys --fingerprint "${SIGNER_FINGERPRINT}"
 echo "$ENCRYPTER_FINGERPRINT:6:" | gpg --import-ownertrust
 echo "$SIGNER_FINGERPRINT:6:" | gpg --import-ownertrust
-
-/usr/bin/duplicity-full-backup
